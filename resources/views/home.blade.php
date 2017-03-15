dcec76741e6ab2170145c66ce1a4ba38e626bf74
jacob <zorluo@163.com>
2017-03-15 12:11:27 +0800

    remove some photos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,7 +70,7 @@
     <!-- /.container -->
     <div class="col-xs-12 text-center abcen wow fadeIn">
         <div class="button_down ">
-            <a class="imgcircle wow bounceInUp" data-wow-duration="1.5s"  href="#whatis"> <img class="img_scroll" src="img/icon/circle.png" alt=""> </a>
+            <a class="imgcircle wow bounceInUp" data-wow-duration="1.5s"  href="#About"> <img class="img_scroll" src="img/icon/circle.png" alt=""> </a>
         </div>
     </div>
 </div>
