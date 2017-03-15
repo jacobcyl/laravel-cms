6b771554a680180d0ec236e64b90a8f0426b5bd5
jacob <zorluo@163.com>
2017-03-15 17:38:20 +0800

    layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -49,7 +49,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">PlurJan Air</a>
+            <a class="navbar-brand" target="_blank" href="/">PlurJan Air</a>
         </div>
         <!-- Top Menu Items -->
         <ul class="nav navbar-right top-nav">
@@ -147,7 +147,7 @@
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+                            <a href="{{ route('dashboard.message-list') }}"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
                         </li>
                         <li>
                             <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
--- resources/views/news-show.blade.php
+++ resources/views/news-show.blade.php
@@ -6,6 +6,11 @@
             <div class="col-md-10 col-md-offset-1">
                 <h4>{{ $post->title }}</h4>
                 <div>
+                    @if($post->cover)
+                        <div style="position: relative; margin-bottom: 15px;width:100%;">
+                            <img class="scale-box" style="width:100%" src="{{ url( $post->cover->path ) }}"  />
+                        </div>
+                    @endif
                     {!! $post->content !!}
                 </div>
             </div>
