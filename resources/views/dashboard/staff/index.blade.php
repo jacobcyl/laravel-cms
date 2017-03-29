1f2c4ca0428a57332c0a3acd8ee2fe8f271917d1
jacob-mac <zorluo@163.com>
2017-03-30 00:36:16 +0800

    。。

--- resources/views/dashboard/staff/index.blade.php
+++ resources/views/dashboard/staff/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                <small>文章列表</small>
+                <small>队员列表</small>
             </h1>
         </div>
     </div>
